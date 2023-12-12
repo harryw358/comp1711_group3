92e9a265fa8275967d23afb5b573223c56fa8dc3
Harry Wray <146842403+harryw358@users.noreply.github.com>
2023-12-12 14:03:08 +0000

    Task 3 progression

--- coursework/FitnessDataSorter.c
+++ coursework/FitnessDataSorter.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 // Define the struct for the fitness data
 typedef struct {
@@ -25,13 +26,13 @@ void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *s
     }
 }
 
-
 int main() {
     FitnessData data[300];
+    int recordCount = 0;
 
     char date[11];
     char time[6];
-    char steps[4];
+    int steps;
 
     int bufferSize = 100;
     char line[bufferSize];
@@ -53,12 +54,12 @@ int main() {
     // declares a bufferSize variable which is the max length of a CSV record
     char lineBuffer[bufferSize];
     while (fgets(lineBuffer, bufferSize, file) != NULL) {
-        tokeniseRecord(lineBuffer, ",", date, time, steps);
+        tokeniseRecord(lineBuffer, ',', date, time, &steps);
 
-        FITNESS_DATA newFitnessData;
+        FitnessData newFitnessData;
         strcpy(newFitnessData.date, date);
         strcpy(newFitnessData.time, time);
-        newFitnessData.steps = atoi(steps);
+        newFitnessData.steps = steps;
 
         data[recordCount] = newFitnessData;
         recordCount++;
@@ -66,5 +67,28 @@ int main() {
 
     // closes the CSV file
     fclose(file);
-    
+
+    // sorts the array
+    // I based this on an idea from https://www.geeksforgeeks.org/bubble-sort/
+    bool swapped = false;
+    for (int i = 0; i < recordCount; i++) {
+        swapped = false;
+        for (int j = 0; j < recordCount - i; j++) {
+            if (data[j].steps > data[j - 1].steps) {
+                FitnessData temp = data[j];
+                data[j - 1] = data[j];
+                data[j] = temp;
+                swapped = true;
+            }
+        }
+    }
+
+    // checks that the array is sorted
+    for (int i = 0; i < recordCount; i++) {
+        printf("%s\n", data[i].date);
+        printf("%s\n", data[i].time);
+        printf("%d\n\n", data[i].steps);
+    }
+
+    return 0;
 }
